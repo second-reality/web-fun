1a3e7c83e5a9d7d9e0654ca201abb56ca308b8cc
Pierrick Bouvier <pierrick.bouvier@allegrodvt.com>
2021-11-03 10:15:26 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,60 @@
 use seed::{prelude::*, *};
+use web_sys::HtmlCanvasElement;
 
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model { counter: 0 }
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    orders.after_next_render(|_| Msg::Rendered);
+    Model::default()
 }
 
 struct Model {
     counter: i32,
+    canvas: ElRef<HtmlCanvasElement>,
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            counter: 0,
+            canvas: ElRef::<HtmlCanvasElement>::default(),
+        }
+    }
 }
 
 enum Msg {
     Increment,
+    Rendered,
 }
 
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => model.counter += 1,
+        Msg::Increment => {
+            model.counter += 1;
+        }
+        Msg::Rendered => {
+            draw(&model.canvas);
+            orders.after_next_render(|_| Msg::Rendered).skip();
+        }
     }
+}
+
+fn draw(canvas: &ElRef<HtmlCanvasElement>) {
+    let canvas = canvas.get().expect("get canvas element");
+    let ctx = seed::canvas_context_2d(&canvas);
+
+    // clear canvas
+    ctx.begin_path();
+    ctx.clear_rect(0., 0., 400., 200.);
+
+    let width = 200.;
+    let height = 100.;
+
+    ctx.rect(0., 0., width, height);
+    ctx.set_fill_style(&JsValue::from_str("blue"));
+    ctx.fill();
+
+    ctx.move_to(0., 0.);
+    ctx.line_to(width, height);
+    ctx.stroke();
 }
 
 fn view(model: &Model) -> Node<Msg> {
